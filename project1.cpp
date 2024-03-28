0749f88d73a4adc655ad9c14092bb06baa20a777
kchristin22 <christinakoutsou22@gmail.com>
2024-03-28 15:52:25 +0200

    Add pop-up menu options and handling and use double buffering

--- project1.cpp
+++ project1.cpp
@@ -4,20 +4,11 @@
 #include <iostream>
 #include <GL/glut.h>
 
-double round_100(double value)
-{
-    uint16_t round_val = (uint16_t)round(value);
+uint8_t n = 6;     /* number of vertices */
+float r = 1 / 3.0; /* ratio of distance from point to vertex */
+size_t num_iter = 12000; /* number of points */
 
-    if (round_val == 250)
-        return 250.0;
-    else if (round_val % 5 == 0) // perfect multiple of 5
-        return (double)round_val;
-
-    uint16_t rounded_100 = ((round_val + 50) / 100) * 100;
-    return (double)rounded_100;
-}
-
-void myinit(void)
+void myinit()
 {
 
     /* attributes */
@@ -34,12 +25,8 @@ void myinit(void)
     glMatrixMode(GL_MODELVIEW);
 }
 
-void display(void)
+void display()
 {
-
-    uint8_t n = 6;         /* number of vertices */
-    float r = 1 / 3.0; /* ratio of distance from point to vertex */
-
     /* define a point data type */
 
     typedef GLfloat point2D[2];
@@ -50,18 +37,17 @@ void display(void)
     // is its vertical distance to the bottom line of the polygon.
     // Else, max vertical distnace is 2*radius_y(= 500)
     double radius_y = n % 2 ? 500.0 / (1 + cos(M_PI / n)) : 250.0;
-    // First point on the left side of the window must be at distance 500 / 2 from the center 
+    // First point on the left side of the window must be at distance 500 / 2 from the center
     double radius_x = 250 / abs(sin(2 * M_PI * 1 / n));
     // Vertical distance of center to the bottom line of the polygon
     double offset_y = 500 - radius_y;
     // Horizontal distance of center to the left line of the polygon
-    double offset_x = 250.0; 
+    double offset_x = 250.0;
 
     for (size_t i = 0; i < n; i++)
     {
         pol_vertices[i][1] = offset_y + radius_y * cos(2 * M_PI * i / n);
         pol_vertices[i][0] = offset_x + radius_x * sin(2 * M_PI * i / n);
-        // std::cout << pol_vertices[i][0] << " " << pol_vertices[i][1] << std::endl;
     }
 
     point2D p = {250.0, 250.0}; /* Initial point inside the polygon (center of window) */
@@ -70,7 +56,7 @@ void display(void)
 
     /* computes and plots 5000 new points */
 
-    for (size_t k = 0; k < 12000; k++)
+    for (size_t k = 0; k < num_iter; k++)
     {
         uint8_t j = rand() % n; /* pick a vertex at random */
 
@@ -82,11 +68,50 @@ void display(void)
         /* plot new point */
 
         glBegin(GL_POINTS);
-        glVertex2fv(p);
+            glVertex2fv(p);
         glEnd();
     }
+    glutSwapBuffers();
     glFlush(); /* clear buffers */
 }
+
+enum MENU_TYPE : int
+{
+    COLOUR = 1,
+    HEX,
+    PENT,
+    EXIT
+};
+
+void menu(int choice)
+{
+    if (choice == COLOUR)
+    {
+        glColor3f(0.5, 0.5, 0.5);
+        glDrawBuffer(GL_BACK);
+        display();
+    }
+    else if (choice == HEX)
+    {
+        n = 6;
+        r = 1 / 3.0;
+        num_iter = 12000;
+        glDrawBuffer(GL_BACK);
+        display();
+    }
+    else if (choice == PENT)
+    {
+        n = 5;
+        r = 3 / 8.0;
+        num_iter = 12000;
+        glDrawBuffer(GL_BACK);
+        display();
+    }
+    else if (choice == EXIT)
+    {
+        exit(0);
+    }
+};
 
 int main(int argc, char **argv)
 {
@@ -94,11 +119,22 @@ int main(int argc, char **argv)
     /* Standard GLUT initialization */
 
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); /* default, not needed */
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE); /* default, not needed */
     glutInitWindowSize(500, 500);                /* 500 x 500 pixel window */
     glutInitWindowPosition(0, 0);                /* place window top left on display */
-    glutCreateWindow("Sierpinski Gasket");       /* window title */
+    glutCreateWindow("Fractal");                 /* window title */
     glutDisplayFunc(display);                    /* display callback invoked when window opened */
+
+    glutCreateMenu(menu);
+
+    // Add menu items
+    glutAddMenuEntry("Colourful", COLOUR);
+    glutAddMenuEntry("Hexagon, r=1/3", HEX);
+    glutAddMenuEntry("Pentagon, r=3/8", PENT);
+    glutAddMenuEntry("Program Terminate", EXIT);
+
+    // Associate a mouse button with menu
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
 
     myinit(); /* set attributes */
 
