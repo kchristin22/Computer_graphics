774261070c2a9299a2e94baaf0a4d538feaeaa98
kchristin <christinakoutsou22@gmail.com>
2024-12-09 12:26:53 +0200

    Finish part A

--- Project 3/project3.cpp
+++ Project 3/project3.cpp
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <cstdlib>
-#include <unistd.h>
-#include <cmath>
-#include <iostream>
 #include <GL/glut.h>
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
 
 GLuint listIndexRect;
 GLuint listIndexSquare;
@@ -72,7 +72,11 @@ void draw_triangle()
     typedef GLfloat point3D[3];
 
     // triangle on top and front of the house
-    point3D vertices[3] = {{5, 10, 10}, {0, 10 + 5 * sqrt(3), 10}, {-5, 10, 10}}; // top vertice: x = 0, y = 10 + sin60 * 10 = 5 * sqrt(3), z = 10
+    point3D vertices[3] = {
+        {5, 10, 10},
+        {0, 10 + 5 * sqrt(3), 10}, // top vertice: x = 0, y = 10 + sin60
+                                   // * 10 = 10 + 5 * sqrt(3), z = 10
+        {-5, 10, 10}};
 
     // plot triangle
     glBegin(GL_TRIANGLES);
@@ -94,14 +98,14 @@ void myinit()
     // 500 x 500 window with origin lower left
 
     glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();                                     // reset the matrix
-    glOrtho(-250.0, 250.0, -250.0, 250.0, -250.0, 250.0); // near = 250.0, far = -250.0, no perspective
+    glLoadIdentity(); // reset the matrix
+    glOrtho(-50.0, 50.0, -50.0, 50.0, -50.0,
+            50.0); // near = 50.0, far = -50.0, no perspective
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(
-        0, 0, 251, // camera pos, no objects are to be behind the camera
-        0, 0, 250, // camera target, z insignificant
-        0.0, 1.0, 0.0);
+    gluLookAt(0, 0, 51, // camera pos, no objects are to be behind the camera
+              0, 0, 50, // camera target, z insignificant
+              0.0, 1.0, 0.0);
 
     listIndexRect = glGenLists(1);
     if (!listIndexRect)
@@ -140,31 +144,9 @@ void myinit()
 }
 void draw_house()
 {
-    glColor3f(1.0, 0.9725, 0.8627); // light shade of brown for the walls
-
-    // back
-    glPushMatrix();
-    glTranslatef(0.0, 0.0, -20.0); // square is on z = 10
-    glCallList(listIndexSquare);
-    glPopMatrix();
-
-    // front
-    glPushMatrix();
-    glCallList(listIndexSquare);
-    glPopMatrix();
-
-    // left side (x = -5)
-    glPushMatrix();
-    glTranslatef(-10.0, 0.0, 0.0);
-    glCallList(listIndexRect);
-    glPopMatrix();
-
-    // right side (x = 5)
-    glPushMatrix();
-    glCallList(listIndexRect);
-    glPopMatrix();
-
     glColor3f(0.698, 0.13, 0.13); // dark shade of red for the roof
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS,
+                100); // rooftop is metallic (reflective)
 
     // right top
     glPushMatrix();
@@ -192,6 +174,66 @@ void draw_house()
     glTranslatef(0, 0, -20);
     glCallList(listIndexTriangle);
     glPopMatrix();
+
+    glColor3f(1.0, 0.9725, 0.8627); // light shade of brown for the walls
+    GLfloat specular[4] = {0, 0, 0, 1.0};
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR,
+                 specular); // no specular highlight
+    GLfloat diffuse[4] = {1.0, 0.9725, 0.8627, 1.0};
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, diffuse);
+
+    // back
+    glPushMatrix();
+    glTranslatef(0.0, 0.0, -20.0); // square is on z = 10
+    glCallList(listIndexSquare);
+    glPopMatrix();
+
+    // front
+    glPushMatrix();
+    glCallList(listIndexSquare);
+    glPopMatrix();
+
+    // left side (x = -5)
+    glPushMatrix();
+    glTranslatef(-10.0, 0.0, 0.0);
+    glCallList(listIndexRect);
+    glPopMatrix();
+
+    // right side (x = 5)
+    glPushMatrix();
+    glCallList(listIndexRect);
+    glPopMatrix();
+}
+
+void draw_surface()
+{
+    glColor3f(0.0, 0.5, 0.0); // green surface
+
+    glBegin(GL_QUADS);
+    glVertex3f(-50, 0, -50);
+    glVertex3f(-50, 0, 50);
+    glVertex3f(50, 0, 50);
+    glVertex3f(50, 0, -50);
+    glEnd();
+}
+
+void draw_surface_tiles()
+{
+    glColor3f(0.0, 0.5, 0.0); // green surface
+
+    // Divide the surface into 100 tiles: Each row and column of the divided
+    // surface should consist of 10 tiles (10 tiles x 10 tiles = 100). The
+    // surface is 100 x 100 (y=0). Hence, each tile is 10 x 10.
+    for (int i = 0; i < 100; i++)
+    {
+        glBegin(GL_QUADS);
+        GLint bottom_left[3] = {-50 + (i % 10) * 10, 0, -50 + (i / 10) * 10};
+        glVertex3iv(bottom_left);
+        glVertex3i(bottom_left[0], 0, bottom_left[2] + 10);      // top left
+        glVertex3i(bottom_left[0] + 10, 0, bottom_left[2] + 10); // top right
+        glVertex3i(bottom_left[0] + 10, 0, bottom_left[2]);      // bottom right
+        glEnd();
+    }
 }
 
 void display()
@@ -199,10 +241,11 @@ void display()
     glDrawBuffer(GL_BACK);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the window
     glLoadIdentity();
-    // glRotatef(theta, 0.0, 1.0, 0.0);
-    glScalef(10, 10, 10);
+    // glRotatef(90, 1.0, 0.0, 0.0);
+    // glScalef(10, 10, 10);
 
     draw_house();
+    draw_surface();
 
     glutSwapBuffers();
     glFlush(); // clear buffers
@@ -213,11 +256,11 @@ void display2()
     glDrawBuffer(GL_BACK);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the window
     glLoadIdentity();
-    glRotatef(theta, 1.0, 2.0, 2.0);
-    glTranslatef(0.0, 20.0, -8 * b / 10); // move to starting position of rotation
-    glScalef(scale_factor * a, scale_factor * a, scale_factor * a);
+    // glRotatef(90, 1.0, 0.0, 0.0);
+    // glScalef(10, 10, 10);
 
     draw_house();
+    draw_surface_tiles();
 
     glutSwapBuffers();
     glFlush(); // clear buffers
@@ -233,11 +276,12 @@ void rotate()
         direction = DIMINISH;
     else if (scale_factor * a <= 1)
         direction = EXPAND;
-    scale_factor = direction == EXPAND ? scale_factor + 0.05 : scale_factor - 0.05;
+    scale_factor =
+        direction == EXPAND ? scale_factor + 0.05 : scale_factor - 0.05;
     glutPostRedisplay();
 }
 
-void menu(int choice)
+void menu_grass(int choice)
 {
     if (choice == 0)
     {
@@ -254,16 +298,16 @@ int main(int argc, char **argv)
     // Standard GLUT initialization
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-    glutInitWindowSize(1000, 1000);    // 1000 x 1000 pixel window
-    glutInitWindowPosition(0, 0);      // place window top left on display
-    glutCreateWindow("Spinning cube"); // window title
-    glutDisplayFunc(display);          // display callback invoked when window opened
-    // glutIdleFunc(rotate);
+    glutInitWindowSize(1000, 1000); // 1000 x 1000 pixel window
+    glutInitWindowPosition(0, 0);   // place window top left on display
+    glutCreateWindow("The house beyond the pines"); // window title
+    glutDisplayFunc(display); // display callback invoked when window opened
+    glutIdleFunc(rotate);
 
-    glutCreateMenu(menu);
+    glutCreateMenu(menu_grass);
     // Add menu items
-    glutAddMenuEntry("Rotate around cube's center", 0);
-    glutAddMenuEntry("Rotate around (0,1,0)", 1);
+    glutAddMenuEntry("Create grass surface as one entire surface", 0);
+    glutAddMenuEntry("Create grass surface as many tiles", 1);
 
     // Associate a mouse button with menu
     glutAttachMenu(GLUT_RIGHT_BUTTON);
